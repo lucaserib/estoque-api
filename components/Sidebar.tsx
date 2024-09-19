4e3908b21b6b23c905799a551e1555dfc89fbe3a
lucaserib <lucasemanuelribeiro@hotmail.com>
2024-09-19 16:36:00 -0300

    começo da sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,43 +1,64 @@
-// components/Sidebar.tsx
 import Link from "next/link";
+import {
+  FaHome,
+  FaClipboardList,
+  FaBox,
+  FaTruck,
+  FaWarehouse,
+  FaSignOutAlt,
+} from "react-icons/fa";
 
 export const Sidebar = () => {
   return (
-    <div className="w-64 h-screen bg-gray-800 text-white p-4">
+    <div className="w-64 h-screen bg-gray-900 text-white p-4">
       <h2 className="text-2xl font-bold mb-6">Gestão de Estoque</h2>
       <ul>
         <li className="mb-4">
           <Link href="/" legacyBehavior>
-            <a className="block py-2 px-4 rounded hover:bg-gray-700">Home</a>
+            <a className="flex items-center py-2 px-4 rounded hover:bg-gray-700">
+              <FaHome className="mr-3" />
+              Home
+            </a>
           </Link>
         </li>
         <li className="mb-4">
-          <Link href="/pedidos-compra" legacyBehavior>
-            <a className="block py-2 px-4 rounded hover:bg-gray-700">Pedidos</a>
+          <Link href="/pedidos" legacyBehavior>
+            <a className="flex items-center py-2 px-4 rounded hover:bg-gray-700">
+              <FaClipboardList className="mr-3" />
+              Pedidos
+            </a>
           </Link>
         </li>
         <li className="mb-4">
           <Link href="/produtos" legacyBehavior>
-            <a className="block py-2 px-4 rounded hover:bg-gray-700">
+            <a className="flex items-center py-2 px-4 rounded hover:bg-gray-700">
+              <FaBox className="mr-3" />
               Produtos
             </a>
           </Link>
         </li>
         <li className="mb-4">
           <Link href="/fornecedores" legacyBehavior>
-            <a className="block py-2 px-4 rounded hover:bg-gray-700">
+            <a className="flex items-center py-2 px-4 rounded hover:bg-gray-700">
+              <FaTruck className="mr-3" />
               Fornecedores
             </a>
           </Link>
         </li>
         <li className="mb-4">
           <Link href="/estoque" legacyBehavior>
-            <a className="block py-2 px-4 rounded hover:bg-gray-700">Estoque</a>
+            <a className="flex items-center py-2 px-4 rounded hover:bg-gray-700">
+              <FaWarehouse className="mr-3" />
+              Estoque
+            </a>
           </Link>
         </li>
         <li className="mb-4">
           <Link href="/saida" legacyBehavior>
-            <a className="block py-2 px-4 rounded hover:bg-gray-700">Saidas</a>
+            <a className="flex items-center py-2 px-4 rounded hover:bg-gray-700">
+              <FaSignOutAlt className="mr-3" />
+              Saida
+            </a>
           </Link>
         </li>
       </ul>
