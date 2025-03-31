bedaec34b442a93b30088df500c0eee3434d10c3
lucaserib <lucasemanuelribeiro@hotmail.com>
2025-03-31 12:54:15 -0300

    arrumando erro de serverless

--- src/app/api/dashboard/top-produtos/route.ts
+++ src/app/api/dashboard/top-produtos/route.ts
@@ -1,8 +1,6 @@
 import { verifyUser } from "@/helpers/verifyUser";
-import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
+import { prisma } from "../../../../../lib/prisma";
 
 export async function GET(request: NextRequest) {
   try {
